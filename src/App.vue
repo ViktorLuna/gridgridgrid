7caac69f12a1c91eb7d69b16e05d7ea74959823d
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2024-07-29 15:53:45 +0800

    kdwnalkdaldnlkwad

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <div class="player">
       <h2>Player 1</h2>
       <PlayerGrid :player="1" />
+      <PlayerGrid :player="2" />
     </div>
     <div class="player">
       <h2>Player 2</h2>
       <PlayerGrid :player="2" />
+      <PlayerGrid :player="1" />
     </div>
   </div>
 </template>
 
 <script setup>
+import Player from './components/PlayerComponent.vue';
 import PlayerGrid from './components/GridComponent.vue';
 </script>
 
---
+++ src/components/PlayerComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="player">
+      <h2>Player {{ player }}</h2>
+      <GridComponent :gridData="myGrid" @cellClicked="handleCellClick" />
+      <GridComponent :gridData="opponentGrid" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import GridComponent from './GridComponent.vue';
+  
+  const props = defineProps({
+    player: {
+      type: Number,
+      required: true,
+    },
+  });
+  
+  const myGrid = ref([]);
+  const opponentGrid = ref([]);
+  
+  const handleCellClick = (cell) => {
+    console.log(cell)
+  };
+  </script>
